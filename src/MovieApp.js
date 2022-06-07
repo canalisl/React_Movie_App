cc97a9e3f04f851a3a16cefb31327f8ff97895be
bruno <bruno95@naver.com>
2022-06-08 02:36:17 +0900

    modified

--- src/MovieApp.js
+++ src/MovieApp.js
@@ -6,7 +6,7 @@ function MovieApp() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path={process.env.PUBLIC_URL + "/"} element={<Home />} />
+        <Route path="/" element={<Home />} />
         {/* 디테일의 useParams로 변수 값(id)를 넘겨줌 */}
         <Route path="/movie/:id" element={<Detail />} />
       </Routes>
